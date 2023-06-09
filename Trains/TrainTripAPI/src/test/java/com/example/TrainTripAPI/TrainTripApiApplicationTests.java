d74eb7a5119b27445a54b2f4d4267ffe802d4ca4
Maxim Shabanov <204619@edu.fa.ru>
2023-06-09 03:48:32 +0300

    final commit

---
+++ Trains/TrainTripAPI/src/test/java/com/example/TrainTripAPI/TrainTripApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Trains.TrainTripAPI.src.test.java.com.example.TrainTripAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTripApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
